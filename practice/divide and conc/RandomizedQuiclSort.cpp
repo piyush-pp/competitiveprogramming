d8eb42af0c3886262b4beb35c01941434afec81b
piyush-pp <piyush.pande95@gmail.com>
2020-09-01 23:06:34 +0530

    d AND c updated

--- practice/divide and conc/RandomizedQuiclSort.cpp
+++ practice/divide and conc/RandomizedQuiclSort.cpp
@@ -46,6 +46,12 @@ void shuffle(ll *a,ll s,ll e)
     return;
 }
 int main() {
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 	
 		ll n;
 		cin>>n;
